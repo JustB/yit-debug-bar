546d77bb6a1286f5a6175a823ec57e01e5602360
Giustino Borzacchiello <giustinob@gmail.com>
2013-07-21 10:01:48 +0200

    refactor functions

--- admin.js
+++ admin.js
@@ -7,47 +7,30 @@
  */
 jQuery(document).ready(function ($) {
     "use strict";
-    $('#wp-admin-bar-yit-reset-theme-options > a').click(function (e) {
+    function yit_reset_call(e, action, msg) {
         e.preventDefault();
 
         var data = {
-            action: 'reset_theme_options'
+            action: action
         };
 
-        console.log('Deleting theme options...');
+        console.log(msg);
 
         $.post(ajaxurl, data, function () {
-            console.log('Successfully reset Theme Options');
+            console.log('SUCCESS: ' + msg);
         });
+    }
+
+    $('#wp-admin-bar-yit-reset-theme-options > a').click(function (e) {
+        yit_reset_call(e, 'reset_theme_options', 'reset theme options');
     });
 
     $('#wp-admin-bar-yit-delete-cache > a').click(function (e) {
-        e.preventDefault();
-
-        var data = {
-            action: 'delete_cache',
-            die: 1
-        };
-
-        console.log('Deleting cache...');
-
-        $.post(ajaxurl, data, function () {
-            console.log('Successfully deleted cache');
-        });
+        yit_reset_call(e, 'delete_cache', 'delete cache');
     });
 
     $('#wp-admin-bar-yit-delete-resize-images > a').click(function (e) {
-        e.preventDefault();
-
-        var data = {
-            action: 'delete_resized_images'
-        };
-
-        console.log('Deleting resized images...');
-
-        $.post(ajaxurl, data, function () {
-            console.log('Successfully deleted resized images');
-        });
+        yit_reset_call(e, 'delete_resized_images', 'delete resized images');
     });
 
 
